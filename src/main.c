b78f4b499b5a4999183a4caabdd56ec4e31c1a60
Adam DePetris <adamdepetris@pm.me>
2026-02-28 23:33:16 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -5,7 +5,9 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+  char command[1024];
   printf("$ ");
-
+  scanf("%s", &command);
+  printf("%s: command not found\n", command);
   return 0;
 }
