767686434802ced6dc50886a0abd724e5914208f
Adam DePetris <adamdepetris@pm.me>
2026-03-03 10:13:35 -0500

    implement echo, add enum decision making

--- src/main.c
+++ src/main.c
@@ -2,20 +2,55 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
 
+enum command
+{
+  unknown,
+  echo,
+  escape,
+};
+
+enum command parse_command(char *command)
+{
+  char *space = strchr(command, ' ');
+  int command_len;
+  if (space)
+  {
+    command_len = space-command;
+  }
+  else
+  {
+    command_len = (int)strlen(command);
+  }
+
+  if (strncmp(command, "echo", command_len) == 0)
+    return echo;
+  else if (strncmp(command, "escape", command_len) == 0)
+    return escape;
+  else
+    return unknown;
+}
+
+int main(int argc, char *argv[]) {
   char command[1024];
   while (1)
   {
+    setbuf(stdout, nullptr);
     printf("$ ");
-    scanf("%s", &command);
-    if (strcmp(command, "exit") == 0)
+    fgets(command, sizeof(command), stdin);
+
+    switch (parse_command(command))
     {
+    case echo:
+      printf("%s\n", command+5);
+      break;
+    case escape:
+      exit(1);
+      break;
+    case unknown:
+      printf("%s: command not found\n", command);
       break;
     }
-    printf("%s: command not found\n", command);
   }
 
   return 0;
