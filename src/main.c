080cdba42bc95990125e877505458c4e9827e581
Adam DePetris <adamdepetris@pm.me>
2026-03-03 11:36:36 -0500

    implement type builtin

--- src/main.c
+++ src/main.c
@@ -2,33 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 enum command
 {
   unknown,
   echo,
-  escape,
+  exit_command,
+  type,
 };
 
 enum command parse_command(char *command)
 {
   char *space = strchr(command, ' ');
-  int command_len;
-  if (space)
-  {
-    command_len = space-command;
-  }
-  else
-  {
-    command_len = (int)strlen(command);
-  }
+  int command_len = space ? space-command : (int)strlen(command);
+  if (command_len == 0)
+    return unknown;
 
   if (strncmp(command, "echo", command_len) == 0)
     return echo;
-  else if (strncmp(command, "exit", command_len) == 0)
-    return escape;
-  else
-    return unknown;
+  if (strncmp(command, "exit", command_len) == 0)
+    return exit_command;
+  if (strncmp(command, "type", command_len) == 0)
+    return type;
+  return unknown;
 }
 
 int main(int argc, char *argv[]) {
@@ -38,6 +33,7 @@ int main(int argc, char *argv[]) {
     setbuf(stdout, nullptr);
     printf("$ ");
     fgets(command, sizeof(command), stdin);
+    //Trim trailing newline. I don't exactly like this solution, probably gonna have to replace later if we need multi line support.
     command[strcspn(command, "\n")] = '\0';
 
     switch (parse_command(command))
@@ -45,14 +41,19 @@ int main(int argc, char *argv[]) {
     case echo:
       printf("%s\n", command+5);
       break;
-    case escape:
+    case exit_command:
       exit(0);
+      break;
+    case type:
+      if (parse_command(command+5) != unknown)
+        printf("%s is a shell builtin\n", command+5);
+      else
+        printf("%s: command not found\n", command+5);
       break;
     case unknown:
       printf("%s: command not found\n", command);
       break;
     }
   }
-
-  return 0;
 }
+
